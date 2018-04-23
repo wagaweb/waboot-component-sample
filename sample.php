99c799eeaaed41ea73cef3683e15bf8bc2afb060
Riccardo D'Angelo <riccardo@waga.it>
2018-04-23 15:40:00 +0200

    Headers updates

--- sample.php
+++ sample.php
@@ -5,8 +5,8 @@ Description: Sample component
 Category: Layout
 Tags: Tag Sample
 Version: 1.0.0
-Author: WAGA Team <dev@waga.it>
-Author URI: http://www.waga.it
+Author: Waboot Team <info@waboot.io>
+Author URI: http://www.waboot.io
 */
 
 if(!class_exists("\\WBF\\modules\\components\\Component")) return;
